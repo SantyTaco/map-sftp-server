e9b111ac907fe9c8f4d0c4f304224dc2f2f1d3cb
Santiago Taco <staco@OKLAP003.local>
2022-12-14 11:57:33 -0600

    Add S3 upload function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const { timingSafeEqual } = require('crypto');
 const { readFileSync } = require('fs');
 const { inspect } = require('util');
+const { uploadFile } = require('./controller/s3Cotroller')
+const ssh2 = require('ssh2');
 
-const { utils: { parseKey }, Server } = require('ssh2');
 
-const allowedUser = Buffer.from('staco');
-const allowedPassword = Buffer.from('welcome1');
-const allowedPubKey = parseKey(readFileSync('id_rsa.pub'));
+const {utils: {sftp: {OPEN_MODE, STATUS_CODE}}} = ssh2;
+const allowedUser = Buffer.from('foo');
+const allowedPassword = Buffer.from('bar');
+
+const BUCKET = 'santytest';
+const fileName = "test1.txt";
 
 function checkValue(input, allowed) {
   const autoReject = (input.length !== allowed.length);
@@ -19,28 +23,23 @@ function checkValue(input, allowed) {
   return (!autoReject && isMatch);
 }
 
-new Server({
+// This simple SFTP server implements file uploading where the contents get
+// ignored ...
+
+new ssh2.Server({
   hostKeys: [{key:readFileSync('id_rsa'), passphrase:'welcome1'}]
 }, (client) => {
   console.log('Client connected!');
 
   client.on('authentication', (ctx) => {
-    console.log('Client connected!');
     let allowed = true;
     if (!checkValue(Buffer.from(ctx.username), allowedUser))
       allowed = false;
-
+    console.log('ctx.method', ctx.method)
     switch (ctx.method) {
       case 'password':
         if (!checkValue(Buffer.from(ctx.password), allowedPassword))
           return ctx.reject();
-        break;
-      case 'publickey':
-        if (ctx.key.algo !== allowedPubKey.type
-            || !checkValue(ctx.key.data, allowedPubKey.getPublicSSH())
-            || (ctx.signature && allowedPubKey.verify(ctx.blob, ctx.signature) !== true)) {
-          return ctx.reject();
-        }
         break;
       default:
         return ctx.reject();
@@ -55,18 +54,46 @@ new Server({
 
     client.on('session', (accept, reject) => {
       const session = accept();
-      session.once('exec', (accept, reject, info) => {
-        console.log('Client wants to execute: ' + inspect(info.command));
-        const stream = accept();
-        stream.stderr.write('Oh no, the dreaded errors!\n');
-        stream.write('Just kidding about the errors!\n');
-        stream.exit(0);
-        stream.end();
+      session.on('sftp', (accept, reject) => {
+        console.log('Client SFTP session');
+        const openFiles = new Map();
+        let handleCount = 0;
+        const sftp = accept();
+        sftp.on('OPEN', (reqid, filename, flags, attrs) => {
+          // Only allow opening /tmp/foo.txt for writing
+          if (filename !== '/tmp/foo.txt' || !(flags & OPEN_MODE.WRITE))
+            return sftp.status(reqid, STATUS_CODE.FAILURE);
+
+          // Create a fake handle to return to the client, this could easily
+          // be a real file descriptor number for example if actually opening
+          // a file on disk
+          const handle = Buffer.alloc(4);
+          openFiles.set(handleCount, true);
+          handle.writeUInt32BE(handleCount++, 0);
+          sftp.handle(reqid, handle);
+        }).on('WRITE', (reqid, handle, offset, data) => {
+          if (handle.length !== 4
+              || !openFiles.has(handle.readUInt32BE(0))) {
+            return sftp.status(reqid, STATUS_CODE.FAILURE);
+          }
+          sftp.status(reqid, STATUS_CODE.OK);
+          uploadFile(BUCKET, fileName, data.toString("utf-8", 0, 12));
+        }).on('CLOSE', (reqid, handle) => {
+          let fnum;
+          if (handle.length !== 4
+              || !openFiles.has(fnum = handle.readUInt32BE(0))) {
+            return sftp.status(reqid, STATUS_CODE.FAILURE);
+          }
+
+          console.log('Closing file');
+          openFiles.delete(fnum);
+          sftp.status(reqid, STATUS_CODE.OK);
+        });
       });
     });
   }).on('close', () => {
-    console.log('Client disconnected test');
+    console.log('Client disconnected');
   });
-}).listen(8070, function() {
+}).listen(8070, '127.0.0.1', function() {
   console.log('Listening on port ' + this.address().port);
 });
---
+++ src/controller/s3Cotroller.js
@@ -0,0 +1,26 @@
+const aws = require("aws-sdk");
+
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION
+  });
+
+const s3 = new aws.S3();
+
+const uploadFile = (bucketName, fileName, fileData) => {
+    const params = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileData.toString("utf-8", 0, 12)
+      };
+
+      s3.upload(params, function (s3Err, data) {
+        if (s3Err) throw s3Err;
+        console.log(`File uploaded successfully at ${data.Location}`);
+      });
+}
+
+module.exports = {
+    uploadFile,
+  };
